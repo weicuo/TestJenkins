ba03957b3698870b31228a24479cf4b60f1c285d
weicuo <1185184775@qq.com>
2019-11-12 00:19:29 +0800

    创建项目

---
+++ src/main/java/com/atguigu/entities/Apple.java
@@ -0,0 +1,16 @@
+package src.main.java.com.atguigu.entities;
+
+/**
+ * ClassName Apple
+ * Description TODO
+ * Author weicuo
+ * Date 2019/11/11
+ **/
+public class Apple {
+
+  private String appName = "good apple";
+
+  public String getAppName() {
+    return appName;
+  }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page import="src.main.java.com.atguigu.entities.Apple" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<%=new Apple().getAppName()%>>
+</body>
+</html>
